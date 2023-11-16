d86df968f2fb9363216f1b6cbfef69a74e57cdad
imran688-dev <farabiahmedimran543@gmail.com>
2023-11-16 18:57:38 +0600

    problem solving file

---
+++ problems.js
@@ -0,0 +1,139 @@
+// 1) Solve the problem with Javascript  to find the maximum between two numbers. (Hint: take 2 variables and find the maximum number between them)
+
+//Answer:-
+// let nam1 = 30;
+// let nam2 = 55;
+// let nam3 = 587;
+// let nam4 = 305;
+// let nam5 = 505;
+
+// console.log(Math.max(nam1, nam2));
+// console.log(Math.min(nam1, nam2, nam3, nam4, nam5));
+
+// -------------------------------------
+// 2) Solve the problem with Javascript  to check whether a number is negative, positive or zero. (take single variable and check using if esle condition)
+
+// Answer: -
+// let number = 30;
+
+// if (number > 0) {
+//   console.log("This is a positive number")
+// } else if (number < 0) {
+//   console.log("This is a negative number")
+// } else {
+//   console.log("This is Zero")
+// }
+
+
+//---------------------------------------
+// 3) Solve the problem with Javascript  to check whether a number is divisible by 10 or not. (take single variable and check using if esle condition)
+
+//Answer:-
+// let nem = 50;
+
+// if (nem % 10 === 0) {
+//   console.log("This number is divisible by", + 10);
+// } else {
+//   console.log("This number is not divisible by", + 10)
+// }
+
+
+//------------------------------------
+// 4) Solve the problem with Javascript  to check whether a number is even or odd.
+
+// Answer: -
+// let naam = 100;
+
+// if (naam % 2 === 0) {
+//   console.log("This is a Even number", naam)
+// } else {
+//   console.log("This is a Odd number", naam)
+// }
+
+
+//--------------------------------------
+// 5) Solve the problem with Javascript  to check whether a character is in the alphabet or not.
+
+// Answer:-
+// let Char = 'A';
+// if ((Char >= 'A' && Char <= 'Z') || (Char >= 'a' && Char <= 'z')) {
+//   console.log(Char + ' is an alphabet.');
+// } else {
+//   console.log(Char + ' is not an alphabet.');
+// }
+
+//---------------------------------------
+// 6) Solve the problem with Javascript  to check whether the temperature is hot, cold or normal. (30 <=  is hot, 25 >= is cold , anyother value is normal)
+
+// Answer:-
+// let hot = 30;
+// let cold = 25;
+
+// let temperature = (hot + cold) / 2;
+
+// if (temperature >= 30) {
+//   console.log("This wether is hot", temperature);
+// } else if (temperature >= 25) {
+//   console.log("This wether is cold", temperature);
+// } else {
+//   console.log("Another temperature is normal");
+// }
+
+
+
+//------------------------------
+// 7) Solve the problem with Javascript  to find a maximum between three numbers. (Hint: take 3 variables and find the maximum number between them use logical and operator for that)
+
+// Answer:-
+// let i = 2000;
+// let j = 20;
+// let k = 255;
+
+// if (i > j && i > k) {
+//   console.log(i, "is greater than", j, k);
+// } else if (j > i && j < k) {
+//   console.log(j, "is grater than", i, k);
+// } else (k > i && k < j)
+// {
+//   console.log(k, "is greater than", i, j);
+// }
+
+
+
+
+//---------------------------------
+// 8) Write a JavaScript expression using the ternary operator that checks if a number x is even or odd. Store the result in a variable and print “Even Number” or “Odd Number”.
+
+// Answer:-
+// let x = 157;
+// let result = x % 2 === 0 ? "Even" : "Odd"
+// console.log(result);
+
+
+
+//---------------------------------
+// 9) Use the logical AND operator to determine if two variables, numOne and numTwo, are both greater than 30. Print a message to the console reflecting the result.
+
+// Answer:-
+// let numOne = 48;
+// let numTwo = 38;
+
+// if (numOne > 30 && numTwo > 30) {
+//   console.log("Both numOne and numTwo are greater than 30")
+// } else {
+//   console.log("At least one of numOne or numTwo is not greater than 30")
+// }
+
+
+//------------------------------------
+// 10) Write a JavaScript program that uses if-else statements combined with logical operators. The program should check if a person’s age is between 13 and 19. If so, print “Teenager”. Otherwise, print “Not a teenager”. (Hint: Combining If-Else and Logical Operators)
+
+// Answer:-
+// let age = 17;
+
+// if (age >= 13 && age <= 19) {
+//   console.log("This is Teenager");
+// } else {
+//   console.log("This is Not a teenager");
+// }
+
